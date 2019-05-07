178f85f44edb7194508183b7b426b842617d3112
Adam Horvath <adamhorvath108@gmail.com>
2019-05-07 11:12:04 -0700

    auth added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Provider } from 'react-redux'
 import store from '../store'
-
 import Chat from './Chat.js'
+import { AuthProvider, AuthRoute } from "../lib/auth"
+import Login from "./auth/Login"
+import Register from "./auth/Register"
+import '../styles/main.css'
 
 class App extends Component {
   render() {
     return (
+      <AuthProvider>
       <Provider store={store}>
-        <Chat />
+        <Router>
+          <div>
+            {/* public routes */}
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+
+            {/* private routes */}
+            <AuthRoute path="/" exact component={Chat} />
+          </div>
+        </Router>
       </Provider>
+    </AuthProvider>
     )
   }
 }
